22918174ff1ded71e387aea5c0f5215aca6363aa
Nicy <80687351+nicy-nicy@users.noreply.github.com>
2023-09-29 15:34:09 +0100

    Add files via upload

---
+++ 09connectpairs/expected_output/submit_0.06477.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd 23980.1.2.0.06477-tempcopyfiles ; mv odata.23980.1.2 odata; mv start.23980.1.2 start 2>/dev/null ; mv finish.23980.1.2 finish ; rm -f path.info.* ;/sharedscratch/nn320/softwarewales/OPTIM/builds/gfortranlammp/LMPOPTIM > OPTIM.connect.23980.1.2 ; cp path.info  ../path.info.23980.1.2 2>/dev/null  ; cp min.data.info  ../min.data.info.23980.1.2 2>/dev/null
